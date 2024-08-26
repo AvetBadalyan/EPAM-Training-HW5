eb06856b6847b2a21aa8281e2b571a415391dc74
Avet Badalyan <avetbadalyan@gmail.com>
2024-08-26 11:51:39 +0400

    Delete dist directory

--- dist/exceptions/NotEligibleForMortgageException.js
+++
@@ -1,6 +0,0 @@
-export default class NotEligibleForMortgageException extends Error {
-    constructor(message = "Not eligible for mortgage") {
-        super(message);
-        this.name = "NotEligibleForMortgageException";
-    }
-}
--- dist/exceptions/WrongDataException.js
+++
@@ -1,6 +0,0 @@
-export default class WrongDataException extends Error {
-    constructor(message = "Wrong data provided") {
-        super(message);
-        this.name = "WrongDataException";
-    }
-}
